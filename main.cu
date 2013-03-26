6e17223322bd6008aa0e5e8b59c9f1128e99e30f
Ashley <Ashley.john.eldridge@gmail.com>
2013-03-26 14:24:14 +0000

    More clean up

--- main.cu
+++ main.cu
@@ -17,11 +17,11 @@
 #include <device_launch_parameters.h>	//ThreadIdx
 
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__);\
-    return EXIT_FAILURE;}} while(0)
+	printf("Error at %s:%d\n",__FILE__,__LINE__);\
+	return EXIT_FAILURE;}} while(0)
 #define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__);\
-    return EXIT_FAILURE;}} while(0)
+	printf("Error at %s:%d\n",__FILE__,__LINE__);\
+	return EXIT_FAILURE;}} while(0)
 
 /* Cuda kernel to bin the random numbers on the GPU */
 __global__ void histGPU(float* d_numbers, int* d_hist, int n, int bins)
@@ -42,12 +42,12 @@ __global__ void histGPU(float* d_numbers, int* d_hist, int n, int bins)
 int main(int argc, char *argv[])
 {
 	/* General variable declarations */
-    int totalToGenerate = 134217728;
-    curandGenerator_t gen;
+	int totalToGenerate = 134217728;
+	curandGenerator_t gen;
 	const int bins = 10000;
 
 	/* Pointer / array declarations  [device (d_*) host (h_*)]*/
-    float *d_RandomNumbers, *h_RandomNumbers;
+	float *d_RandomNumbers, *h_RandomNumbers;
 	int *d_hist;
 
 	// Histogram storage on host, initialised to zero (0)
@@ -67,25 +67,25 @@ int main(int argc, char *argv[])
 	float elapsedTimeCPU;
 
 	printf("Allocating memory on host and device\n");
-    /* Allocate n floats on host */
-    h_RandomNumbers = (float *)calloc(totalToGenerate, sizeof(float));
+	/* Allocate n floats on host */
+	h_RandomNumbers = (float *)calloc(totalToGenerate, sizeof(float));
 
-    /* Allocate n floats on device */
-    CUDA_CALL(cudaMalloc((void **)&d_RandomNumbers, totalToGenerate*sizeof(float)));
-    CUDA_CALL(cudaMalloc((void **)&d_hist, totalToGenerate*sizeof(int)));
+	/* Allocate n floats on device */
+	CUDA_CALL(cudaMalloc((void **)&d_RandomNumbers, totalToGenerate*sizeof(float)));
+	CUDA_CALL(cudaMalloc((void **)&d_hist, totalToGenerate*sizeof(int)));
 
 	/* Initialise histogram on the device to zero (0) */
 	CUDA_CALL(cudaMemset(d_hist, 0, bins*sizeof(int)));
 	
-    /* Create pseudo-random number generator */
-    CURAND_CALL(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MRG32K3A));
-    
-    /* Set seed for the random number generator based on current time */
-    CURAND_CALL(curandSetPseudoRandomGeneratorSeed(gen, time(NULL)));
+	/* Create pseudo-random number generator */
+	CURAND_CALL(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_MRG32K3A));
+	
+	/* Set seed for the random number generator based on current time */
+	CURAND_CALL(curandSetPseudoRandomGeneratorSeed(gen, time(NULL)));
 
 	printf("Generating %i random numbers\n", totalToGenerate);
-    /* Generate n floats on device */	
-    CURAND_CALL(curandGenerateNormal(gen, d_RandomNumbers, totalToGenerate, 0.0f, 0.1f));
+	/* Generate n floats on device */	
+	CURAND_CALL(curandGenerateNormal(gen, d_RandomNumbers, totalToGenerate, 0.0f, 0.1f));
 
 	printf("Binning generated numbers on the GPU\n");
 	/* Bin the random numbers on the GPU */
@@ -99,8 +99,8 @@ int main(int argc, char *argv[])
 	cudaEventElapsedTime(&elapsedTimeGPU, startGPU, stopGPU);
 
 	printf("Transfering numbers to the host device\n");
-    /* Copy device memory to host */
-    CUDA_CALL(cudaMemcpy(h_hist, d_hist, bins * sizeof(int), cudaMemcpyDeviceToHost));
+	/* Copy device memory to host */
+	CUDA_CALL(cudaMemcpy(h_hist, d_hist, bins * sizeof(int), cudaMemcpyDeviceToHost));
 	CUDA_CALL(cudaMemcpy(h_RandomNumbers, d_RandomNumbers, totalToGenerate * sizeof(float), cudaMemcpyDeviceToHost));
 
 	printf("Binning generated numbers on the CPU\n");
@@ -116,16 +116,16 @@ int main(int argc, char *argv[])
 	elapsedTimeCPU = (stopCPU - startCPU)/ CLOCKS_PER_SEC;
 
 	/* Print the times taken to bin the data */
-    
+	
 	printf("\nTime taken to bin the data on the GPU: %fms\n", elapsedTimeGPU);
 	printf("Time taken to bin the data on the CPU: %fs\n", elapsedTimeCPU);
 
 	/* Cleanup */
-    CURAND_CALL(curandDestroyGenerator(gen));
-    CUDA_CALL(cudaFree(d_RandomNumbers));
-    CUDA_CALL(cudaFree(d_hist));
-    free(h_RandomNumbers);    
+	CURAND_CALL(curandDestroyGenerator(gen));
+	CUDA_CALL(cudaFree(d_RandomNumbers));
+	CUDA_CALL(cudaFree(d_hist));
+	free(h_RandomNumbers);    
 
 	getch();
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
